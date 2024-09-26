8455f5d14c44d0087cb425fbeb836d655aecda89
guwls <guswls2654@naver.com>
2024-09-27 04:59:03 +0900

    - PointService 기본 기능 작성 - 각 기능에 대한 단위테스트 진행 - 포인트 충전, 사용에 대한 정책 추가 - 한 번에 하나의 요청만 처리할 수 있도록 리팩토링 - 동시성 제어에 대한 통합 테스트 작성

--- src/main/kotlin/io/hhplus/tdd/database/PointHistoryTable.kt
+++ src/main/kotlin/io/hhplus/tdd/database/PointHistoryTable.kt
@@ -9,9 +9,10 @@ import org.springframework.stereotype.Component
  */
 @Component
 class PointHistoryTable {
-    private val table = mutableListOf<PointHistory>()
-    private var cursor: Long = 1L
+    private val table = mutableListOf<PointHistory>() // 포인트 내역을 저장하는 가변 리스트
+    private var cursor: Long = 1L // PointHistory 객체들을 리스트 형태로 관리하여 유저의 트랜잭션 이력을 저장한다.
 
+    // 특정 유저의 포인트 트랜잭션을 추가. 충전, 사용 등과 같은 트랜잭션 기록이 발생할 때, 이 정보를 리스트에 저장한다.
     fun insert(
         id: Long,
         amount: Long,
@@ -19,18 +20,21 @@ class PointHistoryTable {
         updateMillis: Long,
     ): PointHistory {
         Thread.sleep(Math.random().toLong() * 300L)
+        // 새로운 PointHistory 객체를 생성
         val history = PointHistory(
-            id = cursor++,
-            userId = id,
-            amount = amount,
-            type = transactionType,
-            timeMillis = updateMillis,
+            id = cursor++, // 커서 값이 포인트 이력의 고유 ID로 설정되며, 이후 커서 값이 1 증가
+            userId = id, // 해당 유저의 ID
+            amount = amount, // 트랜잭션 금액
+            type = transactionType, // 트랜잭션 종류
+            timeMillis = updateMillis, // 트랜잭션이 발생한 시간
         )
-        table.add(history)
+        table.add(history) // 생성한 PointHistory 객체를 리스트에 추가
         return history
     }
 
+    // 특정 유저의 포인트 트랜잭션 이력을 조회
     fun selectAllByUserId(userId: Long): List<PointHistory> {
+        // 저장된 PointHistory 리스트에서 userId가 주어진 값과 일치하는 모든 내역을 필터링하여 반환
         return table.filter { it.userId == userId }
     }
 }
--- src/main/kotlin/io/hhplus/tdd/point/UserPoint.kt
+++ src/main/kotlin/io/hhplus/tdd/point/UserPoint.kt
@@ -1,7 +1,17 @@
 package io.hhplus.tdd.point
 
-data class UserPoint(
-    val id: Long,
-    val point: Long,
-    val updateMillis: Long,
-)
+data class UserPoint(val id: Long, var point: Long, val updateMillis: Long){
+    fun charge(chargePoint: Long){
+        if(chargePoint + point > 500000) throw IllegalArgumentException("포인트의 합이 500,000점을 초과하여 충전할 수 없습니다.")
+        if(chargePoint <= 0) throw IllegalArgumentException("충전할 금액을 다시 확인하고 입력해주세요.")
+
+        this.point += chargePoint
+    }
+
+    fun use(usePoint: Long){
+        if(usePoint > point) throw IllegalArgumentException("잔고부족으로 포인트를 사용하실 수 없습니다.")
+        if(usePoint <= 0) throw IllegalArgumentException("사용할 금액을 다시 확인하고 입력해주세요.")
+
+        this.point -= usePoint
+    }
+}
